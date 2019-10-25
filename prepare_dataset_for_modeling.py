17e3b2d11af9f5f98b58a0f5dda673a24a195316
Dr. Vural Aksakalli <vaksakalli@gmail.com>
2019-10-25 17:42:23 +1100

    Update prepare_dataset_for_modeling.py

--- prepare_dataset_for_modeling.py
+++ prepare_dataset_for_modeling.py
@@ -1,3 +1,6 @@
+# V. Aksakalli
+# GPL-3.0, 2019
+
 import pandas as pd
 import os
 import io
@@ -7,12 +10,13 @@ from sklearn import preprocessing
 from sklearn.utils import shuffle
 
 
-def prepare_dataset_for_modeling(dataset_name, n_obs=-1, random_state=999, drop_const_columns=True):
+def prepare_dataset_for_modeling(dataset_name, n_ins=-1, random_state=999, drop_const_columns=True, is_classification=True):
     """
     :param dataset_name: name of dataset to be read from github
-    :param n_obs: how many observations to sample (if > 0)
-    :param random_state: seed for sampling observations
-    :param drop_const_columns: drop constant-value columns (after any sampling) (if True)
+    :param n_ins: how many instances to sample (if > 0)
+    :param random_state: seed for (1) sampling instances and (2) shuffling instances at the end
+    :param drop_const_columns: if True, drop constant-value columns (*after* any sampling)
+    :param is_classification: if True, y values will be label-encoded for use in classification models
     :return: x and y NumPy arrays ready for model fitting
     """
 
@@ -27,11 +31,12 @@ def prepare_dataset_for_modeling(dataset_name, n_obs=-1, random_state=999, drop_
     # drop missing values if there are any
     df = df.dropna()
 
-    # sample a smaller subset if n_obs > 0
-    if n_obs > 0:
-        df = df.sample(n=n_obs, replace=False, random_state=random_state)
+    # sample a smaller subset if n_ins > 0
+    if n_ins > 0:
+        df = df.sample(n=n_ins, replace=False, random_state=random_state)
 
     # if needed, you can drop unique-value columns as below
+    # apparently not all unique-value columns are ID columns!
     # ### df = df.loc[:, df.nunique() < df.shape[0]]
     
    if drop_const_columns:
@@ -60,14 +65,16 @@ def prepare_dataset_for_modeling(dataset_name, n_obs=-1, random_state=999, drop_
 
     # scale x between 0 and 1
     x = preprocessing.MinMaxScaler().fit_transform(x)
-    # label-encode y
-    y = preprocessing.LabelEncoder().fit_transform(y)
-    # shuffle data at the end
-    x, y = shuffle(x, y, random_state=321)
+
+    if is_classification:
+            # label-encode y for classification problems
+        y = preprocessing.LabelEncoder().fit_transform(y)
+
+    # shuffle dataset at the end
+    x, y = shuffle(x, y, random_state=random_state)
 
     return x, y
 
 
 # ## how to run this script
 x, y = prepare_dataset_for_modeling('us_census_income_data.csv')
-
